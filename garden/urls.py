f32d1ef328ce8e601b1a809ca25750b8a1933fe8
kurtwall <kwall@kurtwerks.com>
2024-02-15 17:08:57 -0800

    Integrate a recurring event manager

--- garden/urls.py
+++ garden/urls.py
@@ -5,7 +5,7 @@ from django.urls import include, path
 from garden import settings
 
 urlpatterns = (([
-    path("admin/", admin.site.urls),
-    path("", include('plants.urls')),
+    path('admin/', admin.site.urls),
+    path('', include('plants.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
   + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
